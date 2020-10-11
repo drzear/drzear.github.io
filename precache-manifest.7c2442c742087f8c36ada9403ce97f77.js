e1f7e460337ea49aebc9e269a932a08f2bcb785d
daze269279 <david.zear@radissonhotels.com>
2020-10-11 16:11:04 +0200

    :rocket: initial commit

---
+++ precache-manifest.7c2442c742087f8c36ada9403ce97f77.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ea1840e747053156e88894c5a62d42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9cd28c0c54bc7c65d2c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9b38f327dae9b5d00a1a",
+    "url": "/static/css/main.ae8dc66e.chunk.css"
+  },
+  {
+    "revision": "c9cd28c0c54bc7c65d2c",
+    "url": "/static/js/2.ab959118.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ab959118.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b38f327dae9b5d00a1a",
+    "url": "/static/js/main.db4f2e41.chunk.js"
+  },
+  {
+    "revision": "12bd82e084f0e6172eca",
+    "url": "/static/js/runtime-main.4ca4ed86.js"
+  },
+  {
+    "revision": "c903e910e11daa52936eb5dbda72d098",
+    "url": "/static/media/bineBondi.c903e910.JPG"
+  },
+  {
+    "revision": "734733bbc8f53818024166c75e9f45f1",
+    "url": "/static/media/hobbit.734733bb.JPG"
+  },
+  {
+    "revision": "dcdb19c7db417d84ae42a7d725330d36",
+    "url": "/static/media/kaikoura.dcdb19c7.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "932a82eaeae32ee773f0af7b314a8ae8",
+    "url": "/static/media/vanHills.932a82ea.JPG"
+  }
+]);
